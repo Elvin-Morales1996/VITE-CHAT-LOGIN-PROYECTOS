3f34d6fdfda05486df8c11f977f2331e5746db53
Elvin-Morales1996 <elvinmorales.hernandez96@gmail.com>
2023-09-19 18:36:19 -0600

    ruta protegida

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,32 @@
 //ruta protegida
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from '../utils/firebase';
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const auth = getAuth(app)
+  const navigate = useNavigate();
+
+useEffect(() => {
+  onAuthStateChanged(auth, (user) => {
+    if(user){
+      alert("bienvenido");
+    }else{
+      alert("inicie sesion por favor!")
+      navigate("./login");
+    }
+
+
+  })
+
+
+
+
+}, [])
+
+
+
   return (
    <h1 className="text-white">ruta protegida</h1>
   )
